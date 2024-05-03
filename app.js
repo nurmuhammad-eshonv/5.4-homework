f1ca28b85005ec7cc5736f9d67c30bd6938aebcc
nurmuhammad <nurmuhammadeshonov97@gmail.com>
2024-05-03 11:55:07 +0500

    added the change

---
+++ app.js
@@ -0,0 +1,97 @@
+// 1-misol
+// function sum(x,y){
+//     console.log(x+y);
+// }sum(5,6)
+
+// 2-misol
+
+// function toSecunds(m){
+//    console.log(m * 60);
+// }toSecunds(5)
+
+// 3-misol
+// function nextInteger(int) {
+//     console.log(int + 1);
+// }nextInteger(-5)
+
+// 4-misol
+
+// function uchburchakYuzi(asos, balandlik) {
+//     let res = asos * balandlik
+//     console.log(res / 2);
+// }uchburchakYuzi(7,2)
+
+// 5-misol
+
+// function ageToDays(age) {
+//     console.log( `${age}` * 365);
+// }ageToDays(65)
+
+// 6-misol
+// kiritilgan sonni kubi
+// function toCub(son){
+//   console.log(Math.pow(son,3));
+// }toCub(10)
+
+// 7-misol
+// massivni birinchi soni
+
+// let arr = [1,2,3,4]
+// function firstElement(arr) {
+//     console.log(arr[0]);
+// }firstElement(arr)
+
+// 8-misol
+// ikkita argument bir biriga ko'paytirilgani
+
+// function power(x,y) {
+//     console.log(x * y);
+// }power(3,5)
+
+
+// 25-misol
+// .Funskiya soatlar va minutlarni argument sifatida qabul qiladi. Ushbu funkisya soatlar va minutlarni sekundga o’girib ularning yig’indisini qaytarsin.
+
+// function toSecund(hour,min){
+//     let hour1 = hour * 60 * 60
+//     let minut = min * 60
+//     console.log(hour1 + minut);
+// }toSecund(1,3)
+
+// 27-misol
+//  Funskiay 2ta istalgan turdagi qiymatlar qabul qiladi. Agar ushbu 2ta qiymat ham qiymat jihatidan ham ma’lumot turi jihatidan teng bo’lsa rost, aks holda funkisya yolg’on qiymat qaytarsin.
+
+
+// function tengMi(x, y) {
+//     if(x === y){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }tengMi(1,1)
+
+//28-misol
+//.Funksiya boolean qiymat qabul qiladi. Ushbu funksiya boolean qiymatni stringga o’girib qaytarib bersin.
+
+
+// function booleanToString(bool) {
+//     console.log(typeof bool.toString());
+// }booleanToString(true)
+
+// 29-misol
+//.Arrow funksiyasini yarating ushbu funksiya shunchaki berilgan qiymatni qaytarsin.
+
+// let res = (son) => {
+//     return son
+// }
+// console.log(res('aood'));
+
+// 31-misol
+//Oddiy matematik amallar ketma-ketligi string ko’rinishida funksiyaga argument sifatida beriladi. Ushbu funksiya matematik ifodani bajarib natijani qaytarsin.
+
+// function calc(str) {
+//    let res = eval(str)
+// return res
+// }
+// console.log(calc('2+2'));
+
